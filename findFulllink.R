acfd0d4ef2b23cfbd462d06056fd46ac7fb33fcd
Lin He <609329535@qq.com>
2019-02-11 12:32:37 -0600

    Delete findFulllink.R

--- findFulllink.R
+++
@@ -1,8 +0,0 @@
-findFulllink<-function(fulllink,num){
-  fulllink2<-fulllink
-  for (j in 1:length(fulllink[,2])){
-    linkB<-findLink(fulllink[j,2],num)
-    fulllink2<-rbind(fulllink2,linkB)
-  }  
-  return(fulllink2)
-}
